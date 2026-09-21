9378fa7556750ef2d0038a1af27a68832d34edfe
Rishikesh Sherchan <sherchanrishikesh@gmail.com>
2026-09-21 18:14:29 +0545

    feat: wire up downloaded Noto Sans Devanagari and Hind fonts in demo

--- demo/fonts.js
+++ demo/fonts.js
@@ -2,7 +2,10 @@
 // name: whatever you want to see in the dropdown
 // file: the exact filename inside demo/fonts/
 export const FONTS = [
-  // { name: 'Noto Sans Devanagari', file: 'NotoSansDevanagari-Regular.ttf' },
-  // { name: 'Hind', file: 'Hind-Regular.ttf' },
-  // { name: 'Mangal', file: 'Mangal-Regular.ttf' },
+  { name: 'Noto Sans Devanagari Regular', file: 'NotoSansDevanagari-Regular.ttf' },
+  { name: 'Noto Sans Devanagari Medium', file: 'NotoSansDevanagari-Medium.ttf' },
+  { name: 'Noto Sans Devanagari Bold', file: 'NotoSansDevanagari-Bold.ttf' },
+  { name: 'Noto Sans Devanagari Black', file: 'NotoSansDevanagari-Black.ttf' },
+  { name: 'Hind Regular', file: 'Hind-Regular.ttf' },
+  { name: 'Hind Bold', file: 'Hind-Bold.ttf' },
 ];
